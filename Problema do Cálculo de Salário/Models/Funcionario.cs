b5f412deed68f5ecc74adfbc92d221f6f3ab1c6e
fausio luis <fausioluis@live.com>
2020-03-09 12:52:02 +0200

    Add project files.

---
+++ Problema do Cálculo de Salário/Models/Funcionario.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problema_do_Cálculo_de_Salário.Models
+{
+
+    public enum Cargo
+    {
+        DESENVOLVEDOR,
+        DBA,
+        TESTADOR
+    }
+
+    class Funcionario
+    {
+        public String Nome { get; private set; }
+        public double Salario { get; private set; }
+        public Cargo Cargo { get; private set; }
+
+        public Funcionario(String nome, double salario, Cargo cargo)
+        {
+            this.Nome = nome;
+            this.Salario = salario;
+            this.Cargo = cargo;
+        }
+    }
+}
+//• Desenvolvedores possuem 20% de desconto caso seu salário sejamaior do que
+//R$ 3000,0. Caso contrário, o desconto é de 10%.
+//• DBAs e testadores possuem desconto de 25% se seus salários forem maiores
+//do que R$ 2500,0. 15%, em caso contrário.
---
+++ Problema do Cálculo de Salário/TestesUnitarois/CalculadoraDeSalario.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using Problema_do_Cálculo_de_Salário.Models;
+
+namespace Problema_do_Cálculo_de_Salário.TestesUnitarois
+{
+    [TestFixture]
+    class CalculadoraDeSalario
+    {
+
+        private double CalculaSalario(Funcionario funcionario)
+        {
+            if (funcionario.Cargo.Equals(Cargo.DESENVOLVEDOR))
+            {
+                if (funcionario.Salario > 3000)
+                {
+                    return 3200.0;
+                }
+                else
+                {
+                    return 1350.0;
+                }
+            }
+            else
+            {
+                return 425.0;
+            }
+        }
+
+        [Test]
+        public void DeveCalcularSalarioParaDesenvolvedoresComSalarioAbaixoDoLimite()
+        {
+            CalculadoraDeSalario calculadora = new CalculadoraDeSalario();
+
+            Funcionario desenvolvedor = new Funcionario("Mauricio", 1500.0, Cargo.DESENVOLVEDOR);
+            double salario = calculadora.CalculaSalario(desenvolvedor);
+
+            Assert.AreEqual(1500.0 * 0.9, salario, 0.00001);
+        }
+
+
+        [Test]
+        public void DeveCalcularSalarioParaDesenvolvedoresComSalarioAcimaDoLimite()
+        {
+            CalculadoraDeSalario calculadora = new CalculadoraDeSalario();
+            Funcionario desenvolvedor = new Funcionario("Mauricio", 4000.0, Cargo.DESENVOLVEDOR);
+
+            double salario = calculadora.CalculaSalario(desenvolvedor);
+            Assert.AreEqual(4000.0 * 0.8, salario, 0.00001);
+        }
+
+
+        [Test]
+        public void deveCalcularSalarioParaDBAsComSalarioAbaixoDoLimite()
+        {
+            CalculadoraDeSalario calculadora = new CalculadoraDeSalario();
+            Funcionario dba = new Funcionario("Mauricio", 500.0, Cargo.DBA);
+
+            double salario = calculadora.CalculaSalario(dba);
+            Assert.AreEqual(500.0 * 0.85, salario, 0.00001);
+        }
+    }
+}
